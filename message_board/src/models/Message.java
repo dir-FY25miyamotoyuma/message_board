ae2eaf83dfb42fca750670c981df3d0cb25a238c
dir-FY25miyamotoyuma <yuma.miyamoto.u041@dirbato.co.jp>
2025-06-06 16:01:02 +0900

    getallmessagesを追加

--- message_board/src/models/Message.java
+++ message_board/src/models/Message.java
@@ -1,28 +1,43 @@
 package models;
 
 import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
 
 @Entity
+@NamedQueries({
+@NamedQuery(
+name = "getAllMessages",
+query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+)
+})
 @Table(name = "messages")
 public class Message {
 @Id
 @Column(name = "id")
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Integer id;
+
 @Column(name = "title", length = 255, nullable = false)
 private String title;
+
 @Column(name = "content", length = 255, nullable = false)
 private String content;
+
 @Column(name = "created_at", nullable = false)
 private Timestamp created_at;
+
 @Column(name = "updated_at", nullable = false)
 private Timestamp updated_at;
+
 public Integer getId() {
 return id;
 }
